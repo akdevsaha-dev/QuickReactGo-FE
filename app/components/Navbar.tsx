e990b9ef634f065c1fb2d4eeb59a881c51dcb7dc
akdevsaha-dev <akdevsaha.dev@gmail.com>
2026-03-24 23:52:02 +0530

    add nabvar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+export const Navbar = () => {
+    return <header>
+        <nav>
+            <div className="text-xl font-semibold">
+                create-reactgo-app
+            </div>
+            <div className="flex gap-5">
+                <div>Features</div>
+                <div>Docs</div>
+                <div className="flex gap-2">
+                    <img src="https://cdn.simpleicons.org/github?viewbox=auto&size=20" />
+                </div>
+            </div>
+        </nav>
+    </header>
+}
+
+
