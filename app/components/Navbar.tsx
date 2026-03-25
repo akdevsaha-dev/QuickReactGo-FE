db8426b8698a2f48c99a5c6ef2ed9f0ab8ad206e
akdevsaha-dev <akdevsaha.dev@gmail.com>
2026-03-25 23:43:24 +0530

    update readme.md and add the logo in the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image"
+
 export const Navbar = () => {
     return <header>
         <nav>
-            <div className="md:text-xl text-md font-semibold">
+            <div className="flex items-center gap-3 md:text-xl text-md font-semibold">
+                <Image src="/icon.png" alt="Logo" width={32} height={32} />
                 create-reactgo-app
             </div>
             <div className="flex gap-5">
